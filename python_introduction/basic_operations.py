f6615076d8f65ba319efa9c1ace2eab44444eb2b
Khalil Hafz <khalilhafz@gmail.com>
2025-07-09 15:39:46 +0300

    create basic_operations.py for training of arth operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+# Addition
+sum = number1 + number2
+# Subtraction   
+difference = number1 - number2
+# Multiplication
+product = number1 * number2
+print(f"Addition of {number1} and {number2} is: {sum}")
+print(f"Subtraction of {number1} and {number2} is: {difference}")
+print(f"Multiplication of {number1} and {number2} is: {product}")
+
+
+
+
