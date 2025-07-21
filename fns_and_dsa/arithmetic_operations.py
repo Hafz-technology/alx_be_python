cf63ff79e51458dd39f5e1e83a930d33b698c5d4
Khalil Hafz <khalilhafz@gmail.com>
2025-07-21 22:49:54 +0300

    arithmetic_operations file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1, num2, operation):
+    """
+    Perform basic arithmetic operations on two numbers.
+    """
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Cannot divide by zero."
+        return num1 / num2
+    else:
+        return "Invalid operation. Choose from 'add', 'subtract', 'multiply', or 'divide'."
+
+
+
