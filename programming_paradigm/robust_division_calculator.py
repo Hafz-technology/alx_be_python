32bea52f350016b458b43da4605b4180162ecf81
Khalil Hafz <khalilhafz@gmail.com>
2025-08-03 19:29:52 +0300

    programming_paradigm

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
         numerator = float(numerator)
         denominator = float(denominator)
         result = numerator / denominator
-        return f"The result of the division is: {result}"
+        return f"The result of the division is {result}"
     except ValueError:
         return "Error: Please enter numeric values only."
     except ZeroDivisionError:
